23c06bcbf5810b8f9a88b1e48dfa20c733d8dbd0
irinasvetlichnaya <irinasvetlichnaya16@gmail.com>
2025-09-28 00:00:39 +0300

    first commit

---
+++ sign-up.js
@@ -0,0 +1,20 @@
+const users = [];
+
+form.onsubmit = handleSubmit;
+
+function handleSubmit() {
+  const user = Object.fromEntries(new FormData(form));
+  
+  register();
+  saveUsers();
+}
+
+
+function register(user) {
+  users.push(user);
+}
+
+function saveUsers() {
+  const json = JSON.stringify(users);
+  localStorage.users = json;
+}
